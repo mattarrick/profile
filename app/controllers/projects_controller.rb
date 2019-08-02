cedd31cf6b360ff268c71f5b8cfb89dd6c8d6848
Matt Arrick <mattarrick@gmail.com>
2019-08-02 02:07:41 +0000

    Setup links in top nav

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,4 +7,14 @@ class ProjectsController < ApplicationController
     @project = Project.new
   end
 
+  def create
+    Project.create(project_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :description)
+  end
 end
